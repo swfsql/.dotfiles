21a973d57df58401cc616660cacf85f532eb14c6
Thiago Machado <thy@hotmail.com.br>
2013-05-13 21:22:30 +0000

    xmonad OK

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout
+import XMonad.Layout.NoBorders   ( noBorders, smartBorders )
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
@@ -8,12 +10,17 @@ import System.IO
 main = do
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , terminal   = "urxvt"
         , focusFollowsMouse = False
         , normalBorderColor = "#657b83"
         , focusedBorderColor = "#cb4b16"
-        } `additionalKeys`
+        , layoutHook = avoidStruts (
+                    smartBorders $ Tall 1 (3/100) (1/2) |||
+                    noBorders Full |||
+                    Mirror (Tall 1 (3/100) (1/2)))
+        }
+
+       `additionalKeys`
         [ ((mod1Mask, xK_Escape), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
