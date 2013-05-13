06a46220732c4063f8177fb0788248d15197cfda
Thiago Machado <thy@hotmail.com.br>
2013-05-13 17:25:35 +0000

    reinstall

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,22 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , terminal   = "urxvt"
+        , focusFollowsMouse = False
+        , normalBorderColor = "#657b83"
+        , focusedBorderColor = "#cb4b16"
+        } `additionalKeys`
+        [ ((mod1Mask, xK_Escape), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        , ((mod1Mask, xK_d), kill)
+        ]
+
