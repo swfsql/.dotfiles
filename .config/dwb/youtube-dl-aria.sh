11912e2c5127cf229313311eec560fd85d09a7a3
Thiago Machado <thy@hotmail.com.br>
2013-06-30 10:59:52 +0000

    now the youtube thing is working

--- .config/dwb/youtube-dl-aria.sh
+++ .config/dwb/youtube-dl-aria.sh
@@ -1,8 +1,6 @@
 VIDEO_DATA=`youtube-dl -g --get-url --get-filename "$1"`
 FILENAME=`echo "$VIDEO_DATA" | tail -n 1`
 URL=`echo "$VIDEO_DATA" | head -n 1`
-#aria2c -s 16 -o "$FILENAME" "$URL"
-#echo "antes"
 PREF=""
 
 if [ -e "$HOME/down/.yt/old/$FILENAME" ]
@@ -15,31 +13,25 @@ diana --out="${PREF}${FILENAME}" add $URL
 FILE="${HOME}/down/.yt/${PREF}${FILENAME}.aria2"
 
 cont=1
-while [ ! -e "${HOME}/down/.yt/${PREF}${FILENAME}.aria2" ]
-do
-  sleep 1
-  let cont++
-  if [ $cont -ge 120 ]
-  then 
-    break
-  fi
-done
 
-if [ -e "${HOME}/down/.yt/${PREF}${FILENAME}.aria2" ]
-  then
-    echo "existe, poha"
-  else
-    echo "nao existe, caralho"
+if [ ! -e "$FILE" ] && [ ! -e "${HOME}/down/.yt/${PREF}${FILENAME}"  ]
+then
+  while [ ! -e "FILE" ]
+  do
+    sleep 1
+    let cont++
+    if [ $cont -ge 120 ]
+    then 
+      break
+    fi
+  done
 fi
 
-while [ -e "${HOME}/down/.yt/${PREF}${FILENAME}.aria2" ]
+while [ -e "$FILE" ]
 do
   sleep 2
 done
 
-echo "acordou"
-echo pause | mplayer -slave "$HOME/down/.yt/${PREF}${FILENAME}"
+echo pause | mplayer -slave "${HOME}/down/.yt/${PREF}${FILENAME}"
 
-#echo "depois"
-#mplayer /home/thi/down/.yt/"$FILENAME"
 #https://github.com/tobbez/youtube-dl-aria/blob/master/youtube-dl-aria
